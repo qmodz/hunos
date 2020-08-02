d54a0330f57a2fbce4b3763d1cbce8c79372cc1a
qmodz <68419744+qmodz@users.noreply.github.com>
2020-08-02 14:01:34 +0200

    Added InputDriverInterface.

---
+++ base/kernel/drivers/input/input_driver_interface.hpp
@@ -0,0 +1,21 @@
+#ifndef _INPUT_DRIVER_INTERFACE_HPP
+#define _INPUT_DRIVER_INTERFACE_HPP
+
+#include "system/typedefs.hpp"
+
+namespace hunos {
+namespace kernel {
+namespace drivers {
+inline namespace input {
+
+class InputDriverInterface {
+	public:
+		virtual size_t read(void* ptr, size_t size_of_elements, size_t number_of_elements) = 0;
+};
+
+}  // namespace input
+}  // namespace drivers
+}  // namespace kernel
+}  // namespace hunos
+
+#endif
