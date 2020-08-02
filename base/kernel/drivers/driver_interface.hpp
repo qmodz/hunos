c6c1cfaffda0496089dec92ecd414cc17391f455
qmodz <68419744+qmodz@users.noreply.github.com>
2020-08-02 15:00:01 +0200

    Added DriverInterface.

---
+++ base/kernel/drivers/driver_interface.hpp
@@ -0,0 +1,16 @@
+#ifndef _DRIVER_INTERFACE_HPP
+#define _DRIVER_INTERFACE_HPP
+
+namespace hunos {
+namespace kernel {
+namespace drivers {
+
+class DriverInterface {
+	
+};
+
+}  // namespace drivers
+}  // namespace kernel
+}  // namespace hunos
+
+#endif
--- base/kernel/drivers/output/output_driver_interface.hpp
+++ base/kernel/drivers/output/output_driver_interface.hpp
@@ -3,12 +3,14 @@
 
 #include "system/typedefs.hpp"
 
+#include "../driver_interface.hpp"
+
 namespace hunos {
 namespace kernel {
 namespace drivers {
 inline namespace output {
 
-class OutputDriverInterface {
+class OutputDriverInterface : public DriverInterface {
 	public:
 		virtual size_t write(void* ptr, size_t size_of_elements, size_t number_of_elements) = 0;
 };
